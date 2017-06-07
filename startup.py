cfdba2e19224d9f3bc778d78b6d9e28427f92898
heltucosta <heltucosta@hotmail.com>
2017-06-07 16:14:04 -0300

    Alterado para carregar arquivos automaticamente da raiz do usuario do sistema.
    Windows: "C:\Documents and Settings\(usuario)\GISData" ou "C:\Users\(usuario)\GISData"
    Mac: "~/GISData" ou "/Users/(usuario)/GISData"
    Linux: "~/" ou "/home/(usuario)/GISData"

--- startup.py
+++ startup.py
@@ -1,25 +1,24 @@
 from qgis.core import QgsProject, QgsPoint, QgsRasterLayer, QgsMapLayerRegistry
 from PyQt4.QtCore import QFileInfo
 from qgis.utils import iface
-import time
+import time, os
 
 COORD_CENTER = QgsPoint(-5482440, -1884950)
+home_path = os.path.realpath(QgsProject.instance().readPath("./"))
 
 def load_project():
-    layer = QgsRasterLayer("/home/heltucosta/Desktop/GISData/my_file.xml", "map")
+    layer = QgsRasterLayer(home_path + "/GISData/my_file.xml", "map")
     QgsMapLayerRegistry.instance().addMapLayer(layer)
-    while QgsMapLayerRegistry.instance().count() == 0:
-	    pass
-    print("load finished")
+    print("")
     iface.mapCanvas().setCenter(COORD_CENTER)
     iface.mapCanvas().zoomScale(180000)
     update_canvas()
 
 def update_canvas():
-    iface.addRasterLayer("/home/heltucosta/Desktop/GISData/homicidios_gyn.tiff", "homicidios")
-    iface.addVectorLayer('/home/heltucosta/Desktop/GISData/cameras_2016_final.shp', 'cameras', 'ogr')
-    iface.addVectorLayer('/home/heltucosta/Desktop/GISData/expansao_gyn_sirgas.shp', 'expansao', 'ogr')
-    print('fim de update')
+    iface.addRasterLayer(home_path + "/GISData/homicidios_gyn.tiff", "homicidios")
+    iface.addVectorLayer(home_path + '/GISData/cameras_2016_final.shp', 'cameras', 'ogr')
+    iface.addVectorLayer(home_path + '/GISData/expansao_gyn_sirgas.shp', 'expansao', 'ogr')
+    #print('fim de update')
 
 iface.actionShowPythonDialog().trigger()
 iface.initializationCompleted.connect(load_project)
